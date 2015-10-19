c09b97a6a8e8caf716c5d10137b7b296ad424edc
wyb <122812806@qq.com>
2015-10-19 17:58:26 +0800

    初始化补充，将原来的sble整合为mavenproject的一个model

---
+++ learnenglish/src/main/java/cn/wyb/sble/resources/common/SessionFilter.java
@@ -0,0 +1,46 @@
+package cn.wyb.sble.resources.common;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+public class SessionFilter implements Filter, InitializingBean {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+		logger.debug("cn.wyb.sble.resources.common.SessionFilter.doFilter start!");
+		filterChain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
